e248d11cc5d1b413fb2975e0298d2819d81a3cae
SurajSankarsingh <suraj.sankarsingh@hotmail.com>
2021-06-16 00:01:30 -0400

    (feat: setup mongodb database locally for development and connected server to local db)

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const app = require('./app');
+const connectDatabase = require('./config/database');
 
 const dotenv = require('dotenv');
 
 // Setting up config
 dotenv.config({ path: 'backend/config/config.env' });
+
+//Connecting to db
+connectDatabase();
 
 app.listen(process.env.PORT, () => {
   console.log(
