ea29c54f224cf205307478d74cdf85d068355bc8
SurajSankarsingh <suraj.sankarsingh@hotmail.com>
2021-06-16 17:21:56 -0400

    (feat: Backend - added controller for getting a single product and created route, tested on postman)

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -20,3 +20,20 @@ exports.getProducts = async (req, res, next) => {
     products,
   });
 };
+
+//get single product details => /api/v1/product/:id
+exports.getSingleProduct = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message: 'Product not found',
+    });
+  }
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const {
   newProduct,
   getProducts,
+  getSingleProduct,
 } = require('../controllers/product.controller');
 
 router.route('/products').get(getProducts);
+router.route('/product/:id').get(getSingleProduct);
 
 router.route('/product/new').post(newProduct);
 
