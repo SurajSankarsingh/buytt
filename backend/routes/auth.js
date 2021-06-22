21052192198cd84d9d3829061e2776c5b775df74
SurajSankarsingh <suraj.sankarsingh@hotmail.com>
2021-06-22 18:52:08 -0400

    (feat: Backend - added forgot password token and set up route for password recovery, tested logic and added postman route)

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-const { registerUser, loginUser, logout } = require('../controllers/auth.controller');
+const {
+  registerUser,
+  loginUser,
+  logout,
+  forgotPassword,
+} = require('../controllers/auth.controller');
 
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
+
+router.route('/password/forgot').post(forgotPassword);
 
 router.route('/logout').get(logout);
 
