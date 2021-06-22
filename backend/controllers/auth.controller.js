7ce5edba083eb741dbb02bf15ebe997a354c02ee
SurajSankarsingh <suraj.sankarsingh@hotmail.com>
2021-06-21 22:36:58 -0400

    (feat: Backend - added login controller and route, setup jwt token for route and tested postman route)

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -17,9 +17,40 @@ exports.registerUser = catchAsyncErrors(async (req, res, next) => {
     },
   });
 
-  const token = user.getJwtToken()
+  const token = user.getJwtToken();
 
   res.status(201).json({
+    success: true,
+    token,
+  });
+});
+
+//Login user => /api/v1/login
+exports.loginUser = catchAsyncErrors(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  //checks if email or paossword is entered by user
+  if (!email || !password) {
+    return next(new ErrorHandler('Please enter email & password', 400));
+  }
+
+  //finding user in database
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user) {
+    return next(new ErrorHandler('Invalid Email or Password', 401));
+  }
+
+  //checks if password is correct or not
+  const isPasswordMatched = await user.comparePassword(password);
+
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler('Invalid Email or Password', 401));
+  }
+
+  const token = user.getJwtToken();
+
+  res.status(200).json({
     success: true,
     token,
   });
