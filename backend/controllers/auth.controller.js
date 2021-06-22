c1cb065f0cb35281ed8443f38b15192dc4eb2b9e
SurajSankarsingh <suraj.sankarsingh@hotmail.com>
2021-06-21 22:50:06 -0400

    (feat: Backend - added JWT Token file to create and store information in a httpOnly cookie and implemented in auth controller)

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -2,6 +2,7 @@ const User = require('../models/user');
 
 const ErrorHandler = require('../utils/errorHandler');
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
+const sendToken = require('../utils/jwtToken');
 
 //Register a user => /api/v1/registers/user
 exports.registerUser = catchAsyncErrors(async (req, res, next) => {
@@ -17,12 +18,7 @@ exports.registerUser = catchAsyncErrors(async (req, res, next) => {
     },
   });
 
-  const token = user.getJwtToken();
-
-  res.status(201).json({
-    success: true,
-    token,
-  });
+  sendToken(user, 200, res);
 });
 
 //Login user => /api/v1/login
@@ -48,10 +44,5 @@ exports.loginUser = catchAsyncErrors(async (req, res, next) => {
     return next(new ErrorHandler('Invalid Email or Password', 401));
   }
 
-  const token = user.getJwtToken();
-
-  res.status(200).json({
-    success: true,
-    token,
-  });
+  sendToken(user, 200, res);
 });
