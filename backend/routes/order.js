21bbe4ea8427603cf2358844d29abfc12118b4bf
SurajSankarsingh <suraj.sankarsingh@hotmail.com>
2021-06-28 16:01:03 -0400

    (feat: Backend - added controller and route for all orders admin, tested on postman)

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -5,6 +5,7 @@ const {
   newOrder,
   getSingleOrder,
   myOrders,
+  allOrders,
 } = require('../controllers/order.controller');
 
 const { isAuthenticated, authorizedRoles } = require('../middlewares/auth');
@@ -13,5 +14,7 @@ router.route('/order/new').post(isAuthenticated, newOrder);
 
 router.route('/order/:id').get(isAuthenticated, getSingleOrder);
 router.route('/orders/me').get(isAuthenticated, myOrders);
+
+router.route('/admin/orders').get(isAuthenticated, authorizedRoles('admin'), allOrders);
 
 module.exports = router;
