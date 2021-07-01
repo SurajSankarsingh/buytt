2418718b353789af2c57ca0f42400c53089c12dc
SurajSankarsingh <suraj.sankarsingh@hotmail.com>
2021-07-01 16:38:50 -0400

    (feat: Frontend - setup metadata component for custom titles, react router for browser routing and added home component)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,21 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import Header from './components/Layouts/Header';
 import Footer from './components/Layouts/Footer';
+import Home from './components/Home';
 import './App.css';
 
 function App() {
   return (
-    <div className='App'>
-      <Header />
-      <Footer />
-    </div>
+    <Router>
+      <div className='App'>
+        <Header />
+        <div className='container container-fluid'>
+          <Route path='/' component={Home} exact />
+        </div>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
